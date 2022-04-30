c8b2673a43bc6b507dcaf80c3edc271fb36e74da
Miguel da Mota <miguel@damota.de>
2022-04-30 23:42:06 +0200

    :sparkles: first commit

---
+++ imprint.php
@@ -0,0 +1,34 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Imprint | Miguel da Mota</title>
+
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+
+    <body class="dark -theme">
+
+        <?php include_once "./inc/header.php"; ?>
+
+        <section style="margin-top: 2rem">
+            <div class="wrapper">
+
+                <h2>Imprint</h2>
+
+                <p>Miguel da Mota</p>
+                <p>Fennastraße 20</p>
+                <p>48529 Nordhorn</p>
+                <p>Germany</p>
+
+                <br>
+
+                <p>E-Mail: hi@migueldamota.com</p>
+
+            </div>
+        </section>
+
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,41 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Miguel da Mota</title>
+
+        <link rel="stylesheet" href="/css/app.css?">
+    </head>
+
+    <body class="dark -theme">
+
+        <?php include_once "./inc/header.php"; ?>
+
+        <section class="sectionHero">
+            <div class="heroWrapper">
+
+                <h2>Hi! I am <span class="highlight">Miguel</span>. I am <span class="highlight">18 years</span> old and I live in <span class="highlight">Germany</span>.</h2>
+
+                <div class="arrows">
+                    <div class="arrow"></div>
+                    <div class="arrow"></div>
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div class="wrapper">
+
+                <h2>My projects</h2>
+
+                <p>Coming soon...</p>
+                <div class="imagesList">
+                </div>
+
+            </div>
+        </section>
+
+    </body>
+</html>
